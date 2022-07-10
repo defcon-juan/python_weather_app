8711722bd2004251cf7b2787773f179ea0fdfe86
alex <adtaylor107@gmail.com>
2022-07-10 13:58:30 +0100

    command line output update
    Output now styled for CLI.  Moved style config to separate style.py file..

---
+++ style.py
@@ -0,0 +1,10 @@
+# style.py
+
+PADDING = 20
+
+REVERSE = "\033[;7m"
+RESET = "\033[0m"
+
+
+def change_colour(colour):
+    print(colour, end="")
--- weather.py
+++ weather.py
@@ -3,6 +3,7 @@
 import argparse
 import json
 import sys
+import style
 from configparser import ConfigParser
 from urllib import error, parse, request
 
@@ -100,13 +101,28 @@ def get_weather_data(query_url):
         sys.exit("Couldn't read the server response.")
 
 
+def display_weather_info(weather_data, imperial=False):
+    """Prints formatted weather information about a city.  
+
+        Args:
+            Weather_data(dict): API response from OpenWeather by city name
+            imperial (bool): whether or not to use imperial units for the temperature
+
+        More information at https://openweather.org/current#name
+    """
+    city = weather_data["name"]
+    weather_description = weather_data["weather"][0]["description"]
+    temperature = weather_data["main"]["temp"]
+    country = weather_data["sys"]["country"]
+
+    print(f"{city:^{PADDING}}", end="")
+    print(f"\t{weather_description.capitalize()}", end=" ")
+    print(f"({temperature}°{'F' if imperial else 'C'})")
+
+
 if __name__ == "__main__":
     user_args = read_user_cli_args()
     query_url = build_weather_query(user_args.city, user_args.imperial)
     weather_data = get_weather_data(query_url)
 
-    print(
-        f"{weather_data['name']}: "
-        f"{weather_data['weather'][0]['description']} "
-        f"({weather_data['main']['temp']})"
-    )
+    display_weather_info(weather_data, user_args.imperial)
